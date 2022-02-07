3f6948b963a2a2f5c64bae7b2edb88cc49e60b8e
vinhnguyen3003 <khanhvinh30032000@gmail.com>
2022-02-07 12:28:00 +0700

    Update git

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,57 +1,56 @@
-import React, {Component} from 'react';
-import * as actions from './../actions/index';
-import {connect} from 'react-redux';
+import React, { Component } from "react";
+import * as actions from "./../actions/index";
+import { connect } from "react-redux";
 
-
-class Search extends Component{
-  constructor(props){
-    super(props);
-    this.state={
-        keyword:''
+class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            keyword: "",
+        };
     }
-  }
-  onHandleChange=(event)=>{
-    // var target= event.target;
-    // var name = target.name;
-    // var value = target.value;
-    this.setState({
-        keyword:event.target.value.toLowerCase()
-    });
-  }
-  onSearch=()=>{
-    this.props.onSearch(this.state.keyword);
-  }
-  render(){
-    var {search}=this.state;
-    return(
-        <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-            <div className="input-group">
-                <input 
-                    type="text" 
-                    name="search"
-                    className="form-control" 
-                    placeholder="Nhập từ khóa"
-                    value={search}
-                    onChange={this.onHandleChange}
-                />
-                <span onClick={this.onSearch} className="input-group-addon"><i className="fas fa-search"></i>&nbsp;Tìm</span>
+    onHandleChange = (event) => {
+        // var target= event.target;
+        // var name = target.name;
+        // var value = target.value;
+        this.setState({
+            keyword: event.target.value.toLowerCase(),
+        });
+    };
+    onSearch = () => {
+        this.props.onSearch(this.state.keyword);
+    };
+    render() {
+        var { search } = this.state;
+        return (
+            <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                <div className="input-group">
+                    <input
+                        type="text"
+                        name="search"
+                        className="form-control"
+                        placeholder="Nhập từ khóa"
+                        value={search}
+                        onChange={this.onHandleChange}
+                    />
+                    <span onClick={this.onSearch} className="input-group-addon">
+                        <i className="fas fa-search"></i>&nbsp;Tìm
+                    </span>
+                </div>
             </div>
-        </div>
-      );
-  }
+        );
+    }
 }
 
-const mapStateToProps=()=>{
+const mapStateToProps = () => {
+    return {};
+};
+
+const mapDispatchToProps = (dispatch, props) => {
     return {
-
-    }
-}
-
-const mapDispatchToProps=(dispatch,props)=>{
-    return{
-        onSearch: (keyword)=>{
+        onSearch: (keyword) => {
             dispatch(actions.searchTaskTool(keyword));
-        }
-    }
-}
-export default connect(mapStateToProps,mapDispatchToProps) (Search);
+        },
+    };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Search);
